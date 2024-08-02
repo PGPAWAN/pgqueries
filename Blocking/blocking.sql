a352594ec7d1ca11834cb6ee0d48098915d4254b
PGPAWAN <36628918+PGPAWAN@users.noreply.github.com>
2024-08-02 13:53:31 +0530

    Updates queries
    Updates queries

--- Blocking/blocking.sql
+++ Blocking/blocking.sql
@@ -1,3 +1,6 @@
 select pg_blocking_pids(pid), pid AS blocked_pid, state, query, age(now(),query_start) AS duration from pg_stat_activity where array_length(pg_blocking_pids(pid),1) > 0 order by 1,2;  
 
 select pg_blocking_pids(pid), pid AS blocked_pid, state, age(now(),query_start) AS duration from pg_stat_activity where array_length(pg_blocking_pids(pid),1) > 0 order by 1,2; 
+
+
+select a.pi,a.usename,a.query,b.pi as blocking_pid,b.query as blocking_query from pg_stat_activity a join pg_stat_activity b on b.pid=any(pg_blocking_pids(activity.pid));
--- Terminate/terminate.sql
+++ Terminate/terminate.sql
@@ -1,9 +1,9 @@
 --  Kill all the idle connection:
 
-SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'databasename' AND pid <> pg_backend_pid() AND state in ('idle');
+SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname='databasename' AND pid <> pg_backend_pid() AND state in ('idle');
   
 
-SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'databasename' AND pid <> pg_backend_pid() AND state in ('idle') and usename='username';  
+SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname='databasename' AND pid <> pg_backend_pid() AND state in ('idle') and usename='username';  
 
 -- Terminate particular pid
 SELECT pg_terminate_backend(pid);   
